4b66a0a8f1bc1eeb959aa1cc753e9209f61a14c6
zidniahmeed <zidniahmeed@gmail.com>
2023-02-01 22:37:31 +0700

    add daysui

--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -1,26 +1,28 @@
+import Navbar from "@/Components/Navbar"
 import { Head } from "@inertiajs/react"
 import { data } from "autoprefixer"
 
 export default function name(props) {
     return (
-        <>
+
+        <div className="min-h-screen bg-slate-50">
             <Head title={props.title} />
-            <p>{props.description}</p>
-
-            {props.news ? props.news.map((data, i) => {
-                return (
-
-                    <div key={i}>
-                        <hr />
-                        <p>{data.title}</p>
-                        <p>{data.description}</p>
-                        <p>{data.category}</p>
-                        <p>{data.author}</p>
-                        <hr />
-                    </div>
-                )
-            }) : <p>data kosong</p>
-            }
-        </>
+            <div>
+            <Navbar />
+                {props.news ? props.news.map((data, i) => {
+                    return (
+                        <div key={i} className="p-4 m-2 bg-white text-black shadow-md rounded-md">
+                            <hr />
+                            <p className="text-2xl">{data.title}</p>
+                            <p className="text-sm">{data.description}</p>
+                            <p>{data.category}</p>
+                            <p>{data.author}</p>
+                            <hr />
+                        </div>
+                    )
+                }) : <p>data kosong</p>
+                }
+            </div>
+        </div>
     )
 }
